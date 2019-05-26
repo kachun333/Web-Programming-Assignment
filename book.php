e252fb4d2d0507905203b9d74761c97526239558
afzarahnaushad <34685467+afzarahnaushad@users.noreply.github.com>
2019-05-26 20:45:26 +0800

    update

--- book.php
+++ book.php
@@ -2,9 +2,6 @@
 //including the database connection file
 include_once("config.php");
 
-//fetching data in descending order (lastest entry first)
-//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
-//for displaying all books
 $query = mysqli_query($conn, "SELECT BookCover,Title,Author,ISBN FROM books ORDER BY Title"); // using mysqli_query instead
 
 
@@ -38,18 +35,62 @@ $query = mysqli_query($conn, "SELECT BookCover,Title,Author,ISBN FROM books ORDE
 
 <body>
 
-   <?php include 'header.php>'; ?>
+    <header id="header">
+        <!--Menu Button-->
+        <a id="biblio" href="index.html">
+            <h2>Biblio</h2>
+        </a>
+
+        <!-- profile picture -->
+        <div class="dropdown">
+            <img src="media/profile pic.png" role="button" id="profile" data-toggle="dropdown" aria-haspopup="true"
+                aria-expanded="false">
+
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="#">Profile</a>
+                <a class="dropdown-item" href="#">Setting</a>
+                <a class="dropdown-item" href="preview.html">Logout</a>
+            </div>
+        </div>
+    </header>
+
+    <nav class="navigation">
+        <div>
+            <ul>
+                <li class="navigation-item">
+                    <a href="index.html">DASHBOARD</a>
+                </li>
+                <li class="navigation-item active">
+                    <a href="book.php">BOOKS</a>
+                </li>
+
+                <li class="navigation-item">
+                    <a href="Lending/lending.html">LENDING</a>
+                </li>
+
+                <li class="navigation-item">
+                    <a href="member.html">MEMBER</a>
+                </li>
+                <li class="navigation-item">
+                    <a href="statistic.html">STATISTIC</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
 	
 
 	
     <main class="container" >
    
-  <form id="category" action="search.php" method="POST" >
+  <form action="search.php" method="POST" >
 
     <div class="float-center form-group col-md-6">
    
-      <input name = "searchterm" type="text" required class="form-control" placeholder="Search Book Title or Author Name" title="Type in name" aria-label="Search"> <br>
-	 <!--<div class="form-group col-md-6">-->
+      <input name = "searchterm" type="text" required class="form-control" placeholder="Search Book Title or Author Name" 
+	  title="Type in name" aria-label="Search"> <br>
+	 
 		 <select name="searchtype" class="form-control form-group col-md-3">
 			<option value="Author">Author</option>
 			<option value="Title">Title</option>
@@ -57,16 +98,13 @@ $query = mysqli_query($conn, "SELECT BookCover,Title,Author,ISBN FROM books ORDE
  
 		</select>
 	</div>
-       <!-- <input type="submit" name="submit" value="Search"> -->
+       
 	   
         &nbsp; &nbsp; <button type ="submit" name="submit" value="Search" class="btn">Search</button>
       
 	  <div class="float-right">
         <a href="bookSearch.html"><button id="add-book-btn" type="button" class="btn">Add Book</button></a>
       </div> 
-	  
-	  
-	  
 	  
     </form>
 	<br>
@@ -87,27 +125,21 @@ $query = mysqli_query($conn, "SELECT BookCover,Title,Author,ISBN FROM books ORDE
 	
 	while($res = mysqli_fetch_array($query)) { 		
 		echo "<tr>";
-		echo "<td>"; ?><img src="<?php echo $res['BookCover']; ?>" class='dash-newbook'> <?php echo "</td>";
+		echo "<td>"; ?><img src="<?php echo $res['BookCover']; ?>" class="book-cover-search""> <?php echo "</td>";
 		echo "<td>".$res['ISBN']."</td>";
 		echo "<td>".$res['Title']."</td>";
 		echo "<td>".$res['Author']."</td>";		
-		echo "<td><a href=\"bookinfo\bookinfo.php?ISBN=$res[ISBN]\"><button type=\"button\" class=\"btn\">View</button></a> <br><br>  
+		echo "<td><a href=\"bookinfo.php?ISBN=$res[ISBN]\"><button type=\"button\" class=\"btn\">View</button></a> <br><br>  
 		<a href=\"delete.php?ISBN=$res[ISBN]\" onClick=\"return confirm('Are you sure you want to delete?')\"><button type=\"button\" class=\"btn\">Delete</button></a></td>";
 		
 	}
 	//Step 5: Freeing Resources and Closing Connection using mysqli
 		mysqli_close($conn);
-	
 	?>
-	
-	
-	
 	
 </table>
 
     </main>
-
-
 
 
     <footer class="container text-center font-italic">
